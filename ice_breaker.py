779e99412a2849b1de18d155d92b8ed41d135103
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2023-11-09 19:52:48 -0500

    Update ice_breaker.py

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,22 +6,25 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 
 
-if __name__ == "main":
-    print("Hello Langchain")
-    
+if __name__ == "__main__":
+    print("Hello LangChain!")
+
     linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco Udemy")
-    
+
     summary_template = """
-    given the linked information {information} about a person from I want you to create:
-    1. a short summary
-    2. two interesting facts about them
-    """
-    
+         given the Linkedin information {information} about a person from I want you to create:
+         1. a short summary
+         2. two interesting facts about them
+     """
+
     summary_prompt_template = PromptTemplate(
-        input_variables = ["infromation"], template = summary_template
-    ) 
-    llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
-    chain = LLMChain(llm-llm, prompt = summary_prompt_template)
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
-    
-    print(chain.run(information = linkedin_data))
+
+    print(chain.run(information=linkedin_data))
